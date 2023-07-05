27055b0c0035b1be79e34d678c351f11bd0affe0
Evalyne Wambua <evawambua.blessedtobeablessing@gmail.com>
2023-07-05 14:11:35 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,45 +1,79 @@
-#include "main.h"
+#include "holberton.h"
+int _check_asterisk(char *s3, int end_1);
+int _equal_no_aster(char *s4, char *s5, int cont_2);
+int identical_asterisk(char *s6, char *s7, int cont_3, int cont_4);
+
 /**
- * _strlen_recursion - returns the length of a string.
- * @s: string
- * Return: the length of a string.
+ * wildcmp - function that compare two strings and consiere identicals
+ * @s1: char to review
+ * @s2: char to compare
+ *
+ * Return: 1 if identical, 0 else no identical
  */
-int _strlen_recursion(char *s)
+int wildcmp(char *s1, char *s2)
 {
-	if (*s == '\0')
+	int check_asterisk, cont_1 = 0, cont_2 = 0, cont_3 = 0, cont_4 = 0;
+
+	check_asterisk = _check_asterisk(s2, cont_1);
+	if (check_asterisk == 0)
+		return (_equal_no_aster(s1, s2, cont_2));
+	if (check_asterisk == 1)
+		return (identical_asterisk(s1, s2, cont_3, cont_4));
+	return (2);
+}
+/**
+ * _check_asterisk - function that check if there is a asterisk
+ * @s3: char to review
+ * @cont_1: contador
+ *
+ * Return: 1 if there is a *,else 0
+ */
+int _check_asterisk(char *s3, int cont_1)
+{
+	if (s3[cont_1] == '\0')
 		return (0);
-	else
-		return (1 + _strlen_recursion(s + 1));
-}
-
-/**
- * comparator - compares each character of the string.
- * @s: string
- * @n1: smallest iterator.
- * @n2: biggest iterator.
- * Return: .
- */
-int comparator(char *s, int n1, int n2)
-{
-	if (*(s + n1) == *(s + n2))
-	{
-		if (n1 == n2 || n1 == n2 + 1)
-			return (1);
-		return (0 + comparator(s, n1 + 1, n2 - 1));
-	}
-	return (0);
-}
-
-/**
- * is_palindrome - detects if a string is a palindrome.
- * @s: string.
- * Return: 1 if s is a palindrome, 0 if not.
- */
-int is_palindrome(char *s)
-{
-	if (*s == '\0')
+	if (s3[cont_1] == '*')
 		return (1);
-	return (comparator(s, 0, _strlen_recursion(s) - 1));
+	return (_check_asterisk(s3, cont_1 + 1));
 }
-Footer
-© 2023 GitHub, Inc.
+/**
+ * _equal_no_aster - check if is equal with out asterisk
+ * @s4: char to review
+ * @s5: char to review
+ * @cont_2: contador
+ *
+ * Return: 1 if there are equal, else 0
+ */
+int _equal_no_aster(char *s4, char *s5, int cont_2)
+{
+	if (s4[cont_2] != s5[cont_2])
+		return (0);
+	if ((s4[cont_2] == s5[cont_2]) && (s4[cont_2] == '\0'))
+		return (1);
+	if (s4[cont_2] == s5[cont_2])
+		return (_equal_no_aster(s4, s5, cont_2 + 1));
+	return (1);
+}
+/**
+ * identical_asterisk - check if there are identical whit asterisk
+ * @s6: char to review
+ * @s7: char to review
+ * @cont_3: contador
+ * @cont_4: contador
+ *
+ * Return: 1 if s1 y s2 are identical, else 0
+ */
+int identical_asterisk(char *s6, char *s7, int cont_3, int cont_4)
+{
+	if ((s6[cont_3] != s7[cont_4]) && (s7[cont_4] == '*'))
+		cont_4++;
+	else if ((s6[cont_3] == s7[cont_4]) && (s7[cont_4] != '\0'))
+		cont_4++;
+	else if ((s6[cont_3] != s7[cont_4]) && (s7[cont_4 - 1] != '*'))
+		return (0);
+	if (s7[cont_4] == '*' && cont_3 != 0)
+		cont_3--;
+	if ((s6[cont_3] == s7[cont_4]) && (s7[cont_4] == '\0'))
+		return (1);
+	return (identical_asterisk(s6, s7, cont_3 + 1, cont_4));
+}
